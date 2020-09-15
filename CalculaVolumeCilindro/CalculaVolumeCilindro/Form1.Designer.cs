f81650a8e1b232a48844a0be0170824404d2a3d3
Diegoslourenco <diego.lourenco15@gmail.com>
2020-09-15 11:24:23 -0300

    add volume cilindro

---
+++ CalculaVolumeCilindro/CalculaVolumeCilindro/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace CalculaVolumeCilindro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textAltura = new System.Windows.Forms.TextBox();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textRaio = new System.Windows.Forms.TextBox();
+            this.textVolume = new System.Windows.Forms.TextBox();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Altura";
+            // 
+            // textAltura
+            // 
+            this.textAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAltura.Location = new System.Drawing.Point(201, 28);
+            this.textAltura.Name = "textAltura";
+            this.textAltura.Size = new System.Drawing.Size(257, 31);
+            this.textAltura.TabIndex = 0;
+            this.textAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textAltura_KeyPress);
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalcular.Location = new System.Drawing.Point(12, 263);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(136, 50);
+            this.buttonCalcular.TabIndex = 3;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSair.Location = new System.Drawing.Point(322, 263);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(136, 50);
+            this.buttonSair.TabIndex = 5;
+            this.buttonSair.Text = "Sair";
+            this.buttonSair.UseVisualStyleBackColor = true;
+            this.buttonSair.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Raio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Volume";
+            // 
+            // textRaio
+            // 
+            this.textRaio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textRaio.Location = new System.Drawing.Point(201, 93);
+            this.textRaio.Name = "textRaio";
+            this.textRaio.Size = new System.Drawing.Size(257, 31);
+            this.textRaio.TabIndex = 1;
+            this.textRaio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textAltura_KeyPress);
+            // 
+            // textVolume
+            // 
+            this.textVolume.Enabled = false;
+            this.textVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVolume.Location = new System.Drawing.Point(201, 160);
+            this.textVolume.Name = "textVolume";
+            this.textVolume.Size = new System.Drawing.Size(257, 31);
+            this.textVolume.TabIndex = 2;
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(170, 263);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(136, 50);
+            this.buttonLimpar.TabIndex = 4;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 336);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.textVolume);
+            this.Controls.Add(this.textRaio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSair);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.textAltura);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Volume do Cilindro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textAltura;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textRaio;
+        private System.Windows.Forms.TextBox textVolume;
+        private System.Windows.Forms.Button buttonLimpar;
+    }
+}
+
