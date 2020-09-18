43e585d411937b17fb6e9ede63330f8323e411e3
Diegoslourenco <diego.lourenco15@gmail.com>
2020-09-18 16:55:45 -0300

    upadate ec=xercicio

---
+++ Atividade4/ladosTriangulo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ladosTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "A";
+            // 
+            // textA
+            // 
+            this.textA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textA.Location = new System.Drawing.Point(128, 22);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 38);
+            this.textA.TabIndex = 0;
+            this.textA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textA_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "C";
+            // 
+            // textB
+            // 
+            this.textB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textB.Location = new System.Drawing.Point(128, 78);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 38);
+            this.textB.TabIndex = 1;
+            // 
+            // textC
+            // 
+            this.textC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textC.Location = new System.Drawing.Point(128, 135);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 38);
+            this.textC.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(258, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(258, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Verificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(258, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 42);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 179);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lados do Triângulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Atividade4/ladosTriangulo/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ladosTriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textA.Clear();
+            textB.Clear();
+            textC.Clear();
+            textA.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Definindo variáveis
+            double ladoA, ladoB, ladoC;
+
+            // Verificando se os campos estão vazios
+            if (textA.Text == "")
+            {
+                MessageBox.Show("Preencha o campo A!");
+                return;
+            }
+            else if (textB.Text == "")
+            {
+                MessageBox.Show("Preencha o campo B!");
+                return;
+            }
+            else if (textC.Text == "")
+            {
+                MessageBox.Show("Preencha o campo C!");
+                return;
+            }
+
+            // Verificando se os campos estão preenchidos
+            if(double.TryParse(textA.Text, out ladoA) && double.TryParse(textB.Text, out ladoB) && double.TryParse(textC.Text, out ladoC))
+            {
+                if (ladoA < Math.Abs(ladoB - ladoC) || ladoA > ladoB + ladoC)
+                {
+                    MessageBox.Show("Lado A não passa na regra!");
+                    return;
+                }
+                else if (ladoB < Math.Abs(ladoA - ladoC) || ladoB > ladoA + ladoC)
+                {
+                    MessageBox.Show("Lado B não passa na regra!");
+                    return;
+                }
+                else if (ladoC < Math.Abs(ladoA - ladoB) || ladoC > ladoA + ladoB)
+                {
+                    MessageBox.Show("Lado C não passa na regra!");
+                    return;
+                }
+
+                if (ladoA == ladoB && ladoA == ladoC)
+                {
+                    MessageBox.Show("O triângulo é equilátero!");
+                }
+                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                {
+                    MessageBox.Show("O triângulo é isósceles!");
+                }
+                else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+                {
+                    MessageBox.Show("O triângulo é escaleno!");
+                }
+                
+            }
+        }
+
+        private void textA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendKeys.Send("{TAB}");
+                e.Handled = true;
+            }
+        }
+    }
+}
