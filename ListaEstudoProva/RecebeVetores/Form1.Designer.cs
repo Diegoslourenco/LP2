a9903f2c9e2f2e808ea01795817be323c4625672
Diegoslourenco <diego.lourenco15@gmail.com>
2020-11-10 20:57:00 -0300

    exercicio 37 lista prova

---
+++ ListaEstudoProva/RecebeVetores/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RecebeVetores
+{
+    partial class frmVetores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lstbxSaidas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(178, 43);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(147, 24);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "Recebe Vetores";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(83, 125);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 35);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lstbxSaidas
+            // 
+            this.lstbxSaidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbxSaidas.FormattingEnabled = true;
+            this.lstbxSaidas.ItemHeight = 20;
+            this.lstbxSaidas.Location = new System.Drawing.Point(182, 109);
+            this.lstbxSaidas.Name = "lstbxSaidas";
+            this.lstbxSaidas.Size = new System.Drawing.Size(223, 84);
+            this.lstbxSaidas.TabIndex = 3;
+            // 
+            // frmVetores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 247);
+            this.Controls.Add(this.lstbxSaidas);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "frmVetores";
+            this.Text = "Recebe Vetores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ListBox lstbxSaidas;
+    }
+}
+
---
+++ ListaEstudoProva/RecebeVetores/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace RecebeVetores
+{
+    public partial class frmVetores : Form
+    {
+        public frmVetores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int maximoNumero = 10;
+            double[] vetorA = new double[maximoNumero];
+            double[] matrizB = new double[maximoNumero];
+            String textoVetorA = "";
+            String textoMatrizB = "";
+
+            while (i < maximoNumero)
+            {
+                if (Double.TryParse(Interaction.InputBox($"Digite o valor {i + 1} do vetor: ", "Vetor A"),
+                    out double numero))
+                {
+                    vetorA[i] = numero;
+                    textoVetorA += $"{vetorA[i]} ";
+
+                    if (i % 2 == 0)
+                    {
+                        matrizB[i] = numero * 5;
+                        textoMatrizB += $"{matrizB[i]} ";
+                    }
+                    else
+                    {
+                        matrizB[i] = numero + 5;
+                        textoMatrizB += $"{matrizB[i]} ";
+                    }
+
+                    i += 1;
+                }
+                else
+                {
+                    MessageBox.Show("Digite um número válido!");
+                }
+            }
+
+            lstbxSaidas.Items.Add("Vetor A: ");
+            lstbxSaidas.Items.Add(textoVetorA);
+            lstbxSaidas.Items.Add("Matriz B: ");
+            lstbxSaidas.Items.Add(textoMatrizB);
+        }
+    }
+}
