deec8cd90f7bcb9c1b99014085a9d3e51e98102a
Diegoslourenco <diego.lourenco15@gmail.com>
2020-11-11 18:06:07 -0300

    exercicio 36 lista prova

---
+++ ListaEstudoProva/SomaValores/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _36.SomaValores
+{
+    partial class frmSoma
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(155, 82);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 26);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoma.Location = new System.Drawing.Point(166, 126);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(75, 23);
+            this.btnSoma.TabIndex = 1;
+            this.btnSoma.Text = "Soma";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(66, 40);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(335, 20);
+            this.lblTexto.TabIndex = 2;
+            this.lblTexto.Text = "Digite  um número inteiro positivo entre 0 e 50:";
+            // 
+            // frmSoma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 209);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "frmSoma";
+            this.Text = "Soma números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Label lblTexto;
+    }
+}
+
---
+++ ListaEstudoProva/SomaValores/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _36.SomaValores
+{
+    public partial class frmSoma : Form
+    {
+        public frmSoma()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            String mensagemErro = "Digite um número inteiro positivo entre 0 e 50!" +
+                                  "\nDigite um valor novamente!";
+
+            if (Int32.TryParse(txtNumero.Text, out Int32 numeroEntrada))
+            {
+                if ((numeroEntrada > 0) && (numeroEntrada < 50))
+                {
+                    double numeroSaida = 0;
+
+                    for (double i = 0; i < numeroEntrada; i++)
+                    {
+                        numeroSaida += (i + 1) / (i + 2);
+                    }
+
+                    MessageBox.Show($"Valor da soma é: {numeroSaida:N2}");
+                }
+                else
+                {
+                    MessageBox.Show(mensagemErro);
+                }
+            }
+            else
+            {
+                MessageBox.Show(mensagemErro);
+            }
+
+            txtNumero.Text = "";
+            txtNumero.Focus();
+        }
+    }
+}
