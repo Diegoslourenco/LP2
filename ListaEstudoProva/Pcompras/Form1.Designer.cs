224a355e41d9c5758f8eb87753ac299efa84d700
Diegoslourenco <diego.lourenco15@gmail.com>
2020-11-10 23:06:01 -0300

    exercicio 38 lista prova

---
+++ ListaEstudoProva/Pcompras/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Pcompras
+{
+    partial class frmCompras
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxSaidas = new System.Windows.Forms.ListBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstbxSaidas
+            // 
+            this.lstbxSaidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbxSaidas.FormattingEnabled = true;
+            this.lstbxSaidas.ItemHeight = 20;
+            this.lstbxSaidas.Location = new System.Drawing.Point(189, 57);
+            this.lstbxSaidas.Name = "lstbxSaidas";
+            this.lstbxSaidas.Size = new System.Drawing.Size(223, 204);
+            this.lstbxSaidas.TabIndex = 6;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(90, 109);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 35);
+            this.btnIniciar.TabIndex = 5;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(185, 9);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(187, 24);
+            this.lblTexto.TabIndex = 4;
+            this.lblTexto.Text = "Compras da Semana";
+            // 
+            // frmCompras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 273);
+            this.Controls.Add(this.lstbxSaidas);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "frmCompras";
+            this.Text = "Compras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxSaidas;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblTexto;
+    }
+}
+
---
+++ ListaEstudoProva/Pcompras/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Pcompras
+{
+    public partial class frmCompras : Form
+    {
+        public frmCompras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        { 
+            int i;
+            int j;
+            int numeroDiasSemana = 7;
+            int numeroProdutos = 5;
+            double[,] matrizProdutos = new double[numeroDiasSemana, numeroProdutos];
+            double[] totalPorDia = new double[numeroDiasSemana];
+            double totalGeral = 0;
+
+            lstbxSaidas.Items.Clear();
+
+            CultureInfo cultura = new System.Globalization.CultureInfo("pt-BR");
+            var dia = cultura.DateTimeFormat;
+
+            // Alimenta a matriz de semana por produtos
+            for (i = 0; i < numeroDiasSemana; i++)
+            {
+                for (j = 0; j < numeroProdutos; j++)
+                {
+                    if (double.TryParse(Interaction.InputBox($"Digite o valor do produto {j + 1}:",
+                                                              $"{dia.DayNames[(int)DayOfWeek.Sunday + i]}"),
+                                                              out double valor))
+                    {
+                        matrizProdutos[i, j] = valor;
+                    }
+                }
+            }
+
+            // Soma o total por dia e armazena num vetor
+            for (i = 0; i < numeroDiasSemana; i++)
+            {
+                for (j = 0; j < numeroProdutos; j++)
+                {
+                    totalPorDia[i] += matrizProdutos[i, j];
+                }
+            }
+
+            // Total geral
+            for (i = 0; i < numeroDiasSemana; i++)
+            {
+                totalGeral += totalPorDia[i];
+            }
+
+            lstbxSaidas.Items.Add("Total por dia:");
+            for (i = 0; i < numeroDiasSemana; i++)
+            {
+                lstbxSaidas.Items.Add($"{dia.DayNames[(int)DayOfWeek.Sunday + i]}: {totalPorDia[i]}");
+            }
+            lstbxSaidas.Items.Add($"Total Geral: {totalGeral}");
+        }
+    }
+}
