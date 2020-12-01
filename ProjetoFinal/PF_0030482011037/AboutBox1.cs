9a921c197ef071ed598837219341468dc94c9643
Diegoslourenco <diego.lourenco15@gmail.com>
2020-12-01 11:51:28 -0300

    projeto final

--- ProjetoFinal/PF_0030482011037/AboutBox1.cs
+++ ProjetoFinal/PF_0030482011037/AboutBox1.cs
@@ -18,10 +18,10 @@ namespace PF_0030482011037
             this.labelVersion.Text = String.Format("Versão 1.0");
             this.labelCopyright.Text = "Todos os direitos reservados";
             this.labelCompanyName.Text = "Empresa Desenvolvedora Ltda.";
-            this.txtbxDescription.Text = "Projeto desenvolvido por Diego da Silva Lourenço durante" +
-                "o curso Linguagem de Programação 2 da FATEC sorocaba com a professora Denilce Veloso.\n" +
-                " O objetivo do projeto é criar um cadastro de nome de cidades, onde é possível" +
-                "inserir cidades, modificar dados cadastrados e excluir cidades.";
+            this.txtbxDescription.Text = "Projeto desenvolvido por Diego da Silva Lourenço durante " +
+                "o curso Linguagem de Programação 2 da FATEC sorocaba com a professora Denilce Veloso. " +
+                "O objetivo do projeto é criar um cadastro de nome de cidades, onde é possível " +
+                "inserir, modificar e excluir cidades.";
         }
 
         private void okButton_Click(object sender, EventArgs e)
